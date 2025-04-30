516c5154ab162fce511b8026187f6fe7084a5cb0
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-04-29 22:21:57 -0400

    Operators

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/CikeTheBear.java
@@ -0,0 +1,36 @@
+public class CikeTheBear {
+    public static void main(String[] args) {
+        
+
+        // Operadores aritméticos
+
+        System.out.println("Suma: 10 + 3 = " + (10 + 3));
+        System.out.println("Resta: 10 - 3 = " + (10 - 3));
+        System.out.println("Multiplicación: 10 * 3 = " + (10 * 3));
+        System.out.println("División: 10 / 3 = " + (10 / 3));
+        System.out.println("Módulo: 10 % 3 = " + (10 % 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
